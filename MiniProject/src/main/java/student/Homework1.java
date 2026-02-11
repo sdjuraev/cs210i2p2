a884d7561c96a80de0833243e367a4dd50f16ae2
sdjuraev <s.djuraev@newuu.uz>
2026-02-11 21:45:41 +0500

    V1

---
+++ MiniProject/src/main/java/student/Homework1.java
@@ -0,0 +1,26 @@
+package student;
+
+public class Homework1 {
+
+    public double shippingCalculator(int items){
+        //TODO Write your code here for problem 1
+
+    }
+    public boolean isValidTriangle(int a, int b, int c){
+        //TODO Write your code here for problem 2
+
+    }
+    public boolean isPrime(int num){
+        //TODO Write your code here for problem 3
+
+    }
+
+    public  int hex2int(String hex) {
+
+    }
+    public String int2hex(int num) {
+        //TODO Write your code here for problem 4
+    }
+
+
+}
